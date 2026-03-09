5e7b8aa1c3e5e0b1e1e296c2c1acb6f53cf74e0a
stepan41k <russia.zont@gmail.com>
2026-03-10 00:11:23 +0300

    feat: createa pre update

--- internal/ui/update.go
+++ internal/ui/update.go
@@ -1,1 +1,32 @@
 package ui
+
+import tea "github.com/charmbracelet/bubbletea"
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// switch msg := msg.(type) {
+	// case tea.KeyMsg:
+	// 	switch msg.String() {
+	// 	case "ctrl+c", "q":
+	// 		return m, tea.Quit
+	// 	}
+	// case tea.WindowSizeMsg:
+	// 	m.width, m.height = msg.Width, msg.Height
+	// }
+
+	// if m.state == authState {
+	// 	return m.updateAuth(msg)
+	// }
+
+	// return m.updateList(msg)
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case " ":
+			m.Counter++
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
