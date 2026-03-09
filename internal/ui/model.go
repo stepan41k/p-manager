dc592f4b0cfe9126cc6925aa4cb19a124da30770
stepan41k <russia.zont@gmail.com>
2026-03-10 00:11:29 +0300

    create: pre model

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -1,2 +1,25 @@
 package ui
 
+import tea "github.com/charmbracelet/bubbletea"
+
+type sessionState int
+
+const (
+	authState sessionState = iota
+	listState
+	addState
+)
+
+type Model struct {
+	Counter int
+	// state sessionState
+	// list list.Model
+	// masterInput textinput.Model
+	// errorMsg string
+	// width int
+	// height int
+}
+
+func (m Model) Init() tea.Cmd{
+	return nil
+}
