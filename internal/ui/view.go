491e9578e42b16288b043a50a896a7abc8bf0f83
stepan41k <russia.zont@gmail.com>
2026-03-10 00:11:16 +0300

    feat: create pre view

--- internal/ui/view.go
+++ internal/ui/view.go
@@ -1,2 +1,15 @@
 package ui
 
+import (
+	"fmt"
+)
+
+func (m Model) View() string {
+	s := "=== Мой крутой счетчик ===\n\n"
+	s += fmt.Sprintf("   Текущее значение: %d\n\n", m.Counter)
+	s += "---------------------------\n"
+	s += " [Пробел] - увеличить\n"
+	s += " [q]      - выйти\n"
+
+	return s
+}
