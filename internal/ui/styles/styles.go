f519f12326fc9f6ef907dfc33cce6466b143d0c0
stepan41k <russia.zont@gmail.com>
2026-03-20 22:03:22 +0300

    feat: update styles

--- internal/ui/styles/styles.go
+++ internal/ui/styles/styles.go
@@ -1,18 +1,26 @@
 package styles
 
-import "github.com/charmbracelet/lipgloss"
-
-var (
-    MainColor = lipgloss.Color("#7D56F4")
-    ErrorColor = lipgloss.Color("#FF0000")
-    
-    TitleStyle = lipgloss.NewStyle().
-        Background(MainColor).
-        Foreground(lipgloss.Color("230")).
-        Padding(0, 1)
-
-    BoxStyle = lipgloss.NewStyle().
-        Border(lipgloss.RoundedBorder()).
-        BorderForeground(MainColor).
-        Padding(1)
+import (
+	"charm.land/bubbles/v2/list"
+	"charm.land/lipgloss/v2"
 )
+
+type Styles struct {
+	title        lipgloss.Style
+	item         lipgloss.Style
+	selectedItem lipgloss.Style
+	pagination   lipgloss.Style
+	help         lipgloss.Style
+	quitText     lipgloss.Style
+}
+
+func NewStyles(darkBG bool) Styles {
+	var s Styles
+	s.title = lipgloss.NewStyle().MarginLeft(2)
+	s.item = lipgloss.NewStyle().PaddingLeft(4)
+	s.selectedItem = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	s.pagination = list.DefaultStyles(darkBG).PaginationStyle.PaddingLeft(4)
+	s.help = list.DefaultStyles(darkBG).HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	s.quitText = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	return s
+}
