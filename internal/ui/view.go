3f32339429c2b1d6926b46c2b00e6c94646c3909
stepan41k <russia.zont@gmail.com>
2026-03-20 22:03:14 +0300

    feat: update view

--- internal/ui/view.go
+++ internal/ui/view.go
@@ -2,14 +2,16 @@ package ui
 
 import (
 	"fmt"
+
+	tea "charm.land/bubbletea/v2"
 )
 
-func (m Model) View() string {
-	s := "=== Мой крутой счетчик ===\n\n"
-	s += fmt.Sprintf("   Текущее значение: %d\n\n", m.Counter)
-	s += "---------------------------\n"
-	s += " [Пробел] - увеличить\n"
-	s += " [q]      - выйти\n"
-
-	return s
+func (m Model) View() tea.View {
+	if m.choice != "" {
+		return tea.NewView(m.styles.quitText.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice)))
+	}
+	if m.quitting {
+		return tea.NewView(m.styles.quitText.Render("Not hungry? That’s cool."))
+	}
+	return tea.NewView("\n" + m.list.View())
 }
