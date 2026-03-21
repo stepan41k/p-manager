f4c64506208aceb35ad43d33a305589e548d429d
stepan41k <russia.zont@gmail.com>
2026-03-21 11:30:34 +0300

    refactor: change register

--- internal/ui/update.go
+++ internal/ui/update.go
@@ -2,28 +2,28 @@ package ui
 
 import tea "charm.land/bubbletea/v2"
 
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.list.SetWidth(msg.Width)
+		m.List.SetWidth(msg.Width)
 		return m, nil
 
 	case tea.KeyPressMsg:
 		switch keypress := msg.String(); keypress {
 		case "q", "ctrl+c":
-			m.quitting = true
+			m.Quitting = true
 			return m, tea.Quit
 
 		case "enter":
-			i, ok := m.list.SelectedItem().(Item)
+			i, ok := m.List.SelectedItem().(Item)
 			if ok {
-				m.choice = string(i)
+				m.Choice = string(i)
 			}
 			return m, tea.Quit
 		}
 	}
 
 	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
+	m.List, cmd = m.List.Update(msg)
 	return m, cmd
 }
