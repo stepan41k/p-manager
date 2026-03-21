8d996cafeba895cf762ed86e6a3374ecd8ce1da2
stepan41k <russia.zont@gmail.com>
2026-03-21 11:29:09 +0300

    feat: add title and description to item

--- internal/ui/item.go
+++ internal/ui/item.go
@@ -3,8 +3,6 @@ package ui
 type Item string
 
 func (i Item) FilterValue() string { return "" }
-
-// Если вы используете DefaultDelegate, ему также нужны эти методы:
-// func (i passwordItem) Title() string       { return i.title }
-// func (i passwordItem) Description() string { return i.username }
+func (i Item) Title() string       { return ""}
+func (i Item) Description() string { return ""}
 
