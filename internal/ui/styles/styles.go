7a9675a53d45d2567131e94afa3152e862b8e7af
stepan41k <russia.zont@gmail.com>
2026-03-21 11:28:02 +0300

    feat: update styles structure and styles initialization

--- internal/ui/styles/styles.go
+++ internal/ui/styles/styles.go
@@ -6,21 +6,21 @@ import (
 )
 
 type Styles struct {
-	title        lipgloss.Style
-	item         lipgloss.Style
-	selectedItem lipgloss.Style
-	pagination   lipgloss.Style
-	help         lipgloss.Style
-	quitText     lipgloss.Style
+	Title        lipgloss.Style
+	Item         lipgloss.Style
+	SelectedItem lipgloss.Style
+	Pagination   lipgloss.Style
+	Help         lipgloss.Style
+	QuitText     lipgloss.Style
 }
 
 func NewStyles(darkBG bool) Styles {
 	var s Styles
-	s.title = lipgloss.NewStyle().MarginLeft(2)
-	s.item = lipgloss.NewStyle().PaddingLeft(4)
-	s.selectedItem = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
-	s.pagination = list.DefaultStyles(darkBG).PaginationStyle.PaddingLeft(4)
-	s.help = list.DefaultStyles(darkBG).HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	s.quitText = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	s.Title = lipgloss.NewStyle().MarginLeft(2)
+	s.Item = lipgloss.NewStyle().PaddingLeft(4)
+	s.SelectedItem = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	s.Pagination = list.DefaultStyles(darkBG).PaginationStyle.PaddingLeft(4)
+	s.Help = list.DefaultStyles(darkBG).HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	s.QuitText = lipgloss.NewStyle().Margin(1, 0, 2, 4)
 	return s
 }
