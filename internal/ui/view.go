b8723a6b631e64ae751df94b58b4dd3ced6b8a03
stepan41k <russia.zont@gmail.com>
2026-03-27 02:11:05 +0300

    feat: update view

--- internal/ui/view.go
+++ internal/ui/view.go
@@ -4,14 +4,52 @@ import (
 	"fmt"
 
 	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
 )
 
 func (m *Model) View() tea.View {
-	if m.Choice != "" {
-		return tea.NewView(m.Styles.QuitText.Render(fmt.Sprintf("%s? Sounds good to me.", m.Choice)))
+	var content string
+
+	switch m.state {
+	case authState:
+		content = m.authView()
+	case vaultState:
+		content = m.vaultView()
+	case entryState:
+		content = m.entryView()
 	}
-	if m.Quitting {
-		return tea.NewView(m.Styles.QuitText.Render("Not hungry? That’s cool."))
+
+	return tea.NewView(content)
+	// if m.choice != "" {
+	// 	return tea.NewView(m.styles.QuitText.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice)))
+	// }
+	// if m.quitting {
+	// 	return tea.NewView(m.styles.QuitText.Render("Not hungry? That’s cool."))
+	// }
+	// return tea.NewView("\n" + m.list.View())
+}
+
+func (m *Model) authView() string {
+	title := lipgloss.NewStyle().Bold(true).Render("Password Manager")
+
+	errStr := ""
+	if m.errorMessage != "" {
+		errStr += lipgloss.NewStyle().Foreground(lipgloss.Color("9")).Render("\n" + m.errorMessage)
 	}
-	return tea.NewView("\n" + m.List.View())
+
+	return fmt.Sprintf(
+		"\n%s\n\n%s\n%s\n\n(нажмите Enter для входа)",
+		title,
+		m.passInput.View(),
+		errStr,
+	)
+}
+
+func (m *Model) vaultView() string {
+
+	return m.vaultList.View()
+}
+
+func (m *Model) entryView() string {
+	return m.vaultList.View()
 }
