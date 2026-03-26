656789e234244e5e948c27f80fac0dfb6c46d9f6
stepan41k <russia.zont@gmail.com>
2026-03-27 02:09:38 +0300

    fix: update delegate

--- internal/ui/delegate.go
+++ internal/ui/delegate.go
@@ -23,7 +23,7 @@ func (d ItemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 		return
 	}
 
-	str := fmt.Sprintf("%d. %s", index+1, i)
+	str := fmt.Sprintf("%d. %s", index+1, i.title)
 
 	fn := d.styles.Item.Render
 	if index == m.Index() {
