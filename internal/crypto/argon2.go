463778a622dfa766d565c1ea9822762e8589ed45
stepan41k <russia.zont@gmail.com>
2026-04-02 00:30:37 +0300

    feat: add argon2 devire key

--- internal/crypto/argon2.go
+++ internal/crypto/argon2.go
@@ -1,1 +1,10 @@
 package crypto
+
+import (
+	"golang.org/x/crypto/argon2"
+)
+
+func deriveKey(password string, salt []byte) []byte {
+    // time=1, memory=64MB, threads=4, keyLen=32
+	return argon2.IDKey([]byte(password), salt, 1, 64 * 1024, 4, 32)
+}
