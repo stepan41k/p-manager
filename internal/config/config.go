a8eba50d96f99e08ec7e54e811d40e40416d2c3e
stepan41k <russia.zont@gmail.com>
2026-04-09 23:29:51 +0300

    feat: create config parser

---
+++ internal/config/config.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"github.com/zalando/go-keyring"
+)
+
+type Config struct {
+	S3Config S3Config
+}
+
+type S3Config struct {
+	AccessKey string
+	SecretKey string
+	Region    string	`env:"REGION"`
+	Endpoint  string	`env:"ENDPOINT"`
+	Bucket    string	`env:"BUCKET"`
+}
+
+func MustLoad() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, fmt.Errorf("failed to load .env file: %w", err)
+	}
+
+	var cfg Config
+
+	accessKey, secretKey, err := GetS3Credentials()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load keys: %w", err)
+	}
+
+	cfg.S3Config.AccessKey = accessKey
+	cfg.S3Config.SecretKey = secretKey
+
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		return nil, fmt.Errorf("failed to read config: %w", err)
+	}
+
+	return &cfg, nil
+}
+
+func GetS3Credentials() (string, string, error) {
+	service := "example-app"
+
+	accessKey, err := keyring.Get(service, "access-key")
+	if err != nil {
+		return "", "", fmt.Errorf("access key not found in system: %w", err)
+	}
+
+	secretKey, err := keyring.Get(service, "secret-key")
+	if err != nil {
+		return "", "", fmt.Errorf("secret key not found in system: %w", err)
+	}
+
+	return accessKey, secretKey, nil
+}
