0b6d492f07e299dbe18b558751afe52f1e863194
stepan41k <russia.zont@gmail.com>
2026-04-09 23:32:00 +0300

    feat: add updateVault, updateDetails,  updateCreate, saveAndUpload implementation

--- internal/ui/update.go
+++ internal/ui/update.go
@@ -3,24 +3,25 @@ package ui
 import (
 	"charm.land/bubbles/v2/list"
 	tea "charm.land/bubbletea/v2"
+	"github.com/stepan41k/p-manager/internal/crypto"
 )
 
-type vaultLoadedMsg []list.Item 
+type vaultLoadedMsg []list.Item
 
-func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {	
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	if key, ok := msg.(tea.KeyPressMsg); ok {
 		if key.String() == "ctrl+c" {
 			return m, tea.Quit
 		}
-	} 
+	}
 
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.vaultList.SetSize(msg.Width, msg.Height)
 	case tea.KeyPressMsg:
-        if msg.String() == "ctrl+c" {
-            return m, tea.Quit
-        }
+		if msg.String() == "ctrl+c" {
+			return m, tea.Quit
+		}
 	case vaultLoadedMsg:
 		m.vaultList.SetItems(msg)
 		m.vaultList.Title = "My passwords"
@@ -34,8 +35,8 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return newModel, cmd
 	case vaultState:
 		return m.updateVault(msg)
-	case entryState:
-		return m.updateEntry(msg)
+	case stateDetails:
+		return m.updateDetails(msg)
 	}
 
 	return m, nil
@@ -58,7 +59,6 @@ func (m *Model) updateAuth(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 				return m, loadVaultCmd(password)
 
-
 			} else {
 				m.errorMessage = "Неверный пароль!"
 				m.passInput.SetValue("")
@@ -72,36 +72,106 @@ func (m *Model) updateAuth(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m *Model) updateVault(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "enter":
+			selected := m.vaultList.SelectedItem()
 
+			if item, ok := selected.(VaultItem); ok {
+				m.selectedItem = item
+				m.state = stateDetails
+			}
+
+			return m, nil
+		}
+	}
+
+	var cmd tea.Cmd
 	m.vaultList, cmd = m.vaultList.Update(msg)
 
 	return m, cmd
 }
 
-func (m *Model) updateEntry(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
+func (m *Model) updateDetails(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "esc", "backspace":
+			m.state = vaultState
+			return m, nil
+		case "c":
+			//TODO: implement copying into buffer
+			return m, nil
+		}
+	}
 
+	return m, nil
+}
+
+func (m *Model) updateCreate(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "esc":
+			m.state = vaultState
+			return m, nil
+		case "tab", "down":
+			m.inputs[m.focusIndex].Blur()
+			m.focusIndex = (m.focusIndex + 1) % len(m.inputs)
+			m.inputs[m.focusIndex].Focus()
+			return m, nil
+		case "g":
+			if m.focusIndex == 2 {
+				m.inputs[2].SetValue(crypto.GeneratePassword(16))
+			}
+			return m, nil
+		case "enter":
+			if m.focusIndex == len(m.inputs) - 1{
+				newEntry := VaultItem {
+					Resource: m.inputs[0].Value(),
+					Username: m.inputs[1].Value(),
+					Email: m.inputs[2].Value(),
+					Password: m.inputs[3].Value(),
+				}
+				return m, m.saveAndUploadCmd(newEntry)
+			}
+			m.inputs[m.focusIndex].Blur()
+			m.focusIndex++
+			m.inputs[m.focusIndex].Focus()
+			return m, nil
+		}
+	}
+	
+	var cmd tea.Cmd
+	m.inputs[m.focusIndex], cmd = m.inputs[m.focusIndex].Update(msg)
 	return m, cmd
 }
 
+func (m *Model) saveAndUploadCmd(entry VaultItem) tea.Cmd {
+	return func() tea.Msg {
+		// 1. Добавляем новую запись в общий список
+        // (Обычно вы читаете текущий список, добавляете новый и шифруете всё вместе)
+        
+        // 2. Шифруем (заглушка)
+        // encryptedData := crypto.Encrypt(allEntries, m.masterKey)
 
-func loadVaultCmd(password string) tea.Cmd {
-    return func() tea.Msg {
-
-        items := []list.Item{
-            VaultItem{resource: "Github", email: "example@gmail.com", username: "stepan", password: "123456789"},
-            VaultItem{resource: "Google", email: "admin@gmail.com", username: "nickname", password: "55112233"},
-        }
-
-        return vaultLoadedMsg(items)
-    }
+        // 3. Отправляем в S3
+        // err := storage.UploadToS3(encryptedData)
+        
+        // if err != nil { return errorMsg(err) }
+        return vaultLoadedMsg(m.vaultList.Items()) // Возвращаемся в список
+	}
 }
 
-// func loadEntryCmd() tea.Cmd {
-// 	return func() tea.Msg {
-// 		items := []list.Item{
-// 			Item
-// 		}
-// 	}
-// }
+func loadVaultCmd(password string) tea.Cmd {
+	return func() tea.Msg {
+
+		items := []list.Item{
+			VaultItem{Resource: "Github", Email: "example@gmail.com", Username: "stepan", Password: "123456789"},
+			VaultItem{Resource: "Google", Email: "admin@gmail.com", Username: "nickname", Password: "55112233"},
+		}
+
+		return vaultLoadedMsg(items)
+	}
+}
