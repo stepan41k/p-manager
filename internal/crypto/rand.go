1130cf78f68add6b9e08e94fc69508d0890ed87b
stepan41k <russia.zont@gmail.com>
2026-04-09 23:34:14 +0300

    feat: create password generation function

--- internal/crypto/rand.go
+++ internal/crypto/rand.go
@@ -1,3 +1,16 @@
 package crypto
 
-// Генерация случайных паролей
+import "crypto/rand"
+
+func GeneratePassword(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+	
+	b := make([]byte, length)
+	rand.Read(b)
+	
+	for i := range b {
+		b[i] = charset[int(b[i]) % len(charset)]
+	}
+	
+	return string(b)
+}
