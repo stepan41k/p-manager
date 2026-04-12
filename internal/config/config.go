8daa3017559fb223c5e77741e5b1761d2ff41404
stepan41k <russia.zont@gmail.com>
2026-04-12 14:49:57 +0300

    chore: update config parser file

--- internal/config/config.go
+++ internal/config/config.go
@@ -38,19 +38,23 @@ func MustLoad() (*Config, error) {
 	if err := cleanenv.ReadEnv(&cfg); err != nil {
 		return nil, fmt.Errorf("failed to read config: %w", err)
 	}
+	
+	if cfg.S3Config.AccessKey == "" || cfg.S3Config.Bucket == "" || cfg.S3Config.Endpoint == "" || cfg.S3Config.Region == "" || cfg.S3Config.SecretKey == "" {
+		return nil, fmt.Errorf("empty field in config")
+	}
 
 	return &cfg, nil
 }
 
-func GetS3Credentials() (string, string, error) {
+func GetS3Credentials() (accessKey, secretKey string, err error) {
 	service := "vault-app"
 
-	accessKey, err := keyring.Get(service, "access_key")
+	accessKey, err = keyring.Get(service, "access_key")
 	if err != nil {
 		return "", "", fmt.Errorf("access key not found in system: %w", err)
 	}
 
-	secretKey, err := keyring.Get(service, "secret_key")
+	secretKey, err = keyring.Get(service, "secret_key")
 	if err != nil {
 		return "", "", fmt.Errorf("secret key not found in system: %w", err)
 	}
