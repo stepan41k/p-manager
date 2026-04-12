af9facadbac4029323ea1c85bee83fb7c1e648d0
stepan41k <russia.zont@gmail.com>
2026-04-12 14:50:53 +0300

    feat: add logging, remove body.Close()

--- internal/storage/s3/s3.go
+++ internal/storage/s3/s3.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	sconfig "github.com/aws/aws-sdk-go-v2/config"
@@ -14,18 +14,20 @@ import (
 )
 
 type Storage struct {
+	log    *slog.Logger
 	client *s3.Client
 	bucket string
 }
 
-func New(ctx context.Context, cfg config.S3Config) (*Storage, error) {
-	sConfig, err := sconfig.LoadDefaultConfig(context.Background(),
+func New(ctx context.Context, cfg *config.S3Config, log *slog.Logger) (*Storage, error) {
+	sConfig, err := sconfig.LoadDefaultConfig(ctx,
 		sconfig.WithRegion(cfg.Region),
+		sconfig.WithLogger(s3SlogAdapter{log: log}),
 		sconfig.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(cfg.AccessKey, cfg.SecretKey, "")),
 	)
-	
+
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("failed to load default config: %w", err)
 	}
 
 	client := s3.NewFromConfig(sConfig, func(o *s3.Options) {
@@ -34,22 +36,25 @@ func New(ctx context.Context, cfg config.S3Config) (*Storage, error) {
 	})
 
 	return &Storage{
+		log:    log,
 		client: client,
 		bucket: cfg.Bucket,
 	}, nil
 }
 
 func (s *Storage) Upload(ctx context.Context, key string, body io.Reader) error {
+	s.log.Info("Загрузка объекта %s в бакет %s", key, s.bucket)
+
 	_, err := s.client.PutObject(ctx, &s3.PutObjectInput{
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(key),
 		Body:   body,
 	})
-	
+
 	if err != nil {
 		return fmt.Errorf("failed to upload object: %w", err)
 	}
-	
+
 	return nil
 }
 
@@ -58,13 +63,11 @@ func (s *Storage) Download(ctx context.Context, key string) (io.ReadCloser, erro
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(key),
 	})
-	
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to download object: %w", err)
 	}
-	
-	defer result.Body.Close()
-	
+
 	return result.Body, nil
 }
 
@@ -73,10 +76,10 @@ func (s *Storage) Delete(ctx context.Context, key string) error {
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(key),
 	})
-	
+
 	if err != nil {
 		return fmt.Errorf("failed to delete object: %w", err)
 	}
-	
+
 	return nil
 }
