ba3ccd0dd38d5966979b4fa201b6f813f785374f
stepan41k <russia.zont@gmail.com>
2026-04-12 14:50:20 +0300

    refactor: add error handling

--- internal/crypto/rand.go
+++ internal/crypto/rand.go
@@ -6,7 +6,10 @@ func GeneratePassword(length int) string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
 	
 	b := make([]byte, length)
-	rand.Read(b)
+	_, err := rand.Read(b)
+	if err != nil {
+		return ""
+	}
 	
 	for i := range b {
 		b[i] = charset[int(b[i]) % len(charset)]
