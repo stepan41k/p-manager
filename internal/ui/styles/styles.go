a1ac29694ff323d44def7acdb0302f8504a67217
stepan41k <russia.zont@gmail.com>
2026-04-12 14:44:50 +0300

    feat: add styles for details view

--- internal/ui/styles/styles.go
+++ internal/ui/styles/styles.go
@@ -12,15 +12,40 @@ type Styles struct {
 	Pagination   lipgloss.Style
 	Help         lipgloss.Style
 	QuitText     lipgloss.Style
+
+	Card        lipgloss.Style
+	DetailLabel lipgloss.Style
+	DetailValue lipgloss.Style
+	DetailKey   lipgloss.Style
 }
 
 func NewStyles(darkBG bool) Styles {
 	var s Styles
 	s.Title = lipgloss.NewStyle().MarginLeft(2)
 	s.Item = lipgloss.NewStyle().PaddingLeft(4)
-	s.SelectedItem = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	s.SelectedItem = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("120"))
 	s.Pagination = list.DefaultStyles(darkBG).PaginationStyle.PaddingLeft(4)
 	s.Help = list.DefaultStyles(darkBG).HelpStyle.PaddingLeft(4).PaddingBottom(1)
 	s.QuitText = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+
+	s.Card = lipgloss.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("170")).
+		Padding(1, 2).
+		Margin(1, 2).
+		Width(50)
+
+	s.DetailLabel = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("241")).
+		Bold(true).
+		Width(10)
+
+	s.DetailValue = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("255"))
+
+	s.DetailKey = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("255")).
+		Bold(true)
+
 	return s
 }
