d8fd23224e78930b68c04a3b47e0d1559c03d358
stepan41k <russia.zont@gmail.com>
2026-07-28 23:36:18 +0300

    feat(ui): rename file and add keymaps for different states

---
+++ internal/ui/keys.go
@@ -0,0 +1,67 @@
+package ui
+
+import (
+	"charm.land/bubbles/v2/key"
+)
+
+type KeyMap struct {
+	Auth    authKeyMap
+	Vault   vaultKeyMap
+	Details detailsKeyMap
+	Form    formKeyMap
+}
+
+type authKeyMap struct {
+	Enter key.Binding
+	Quit  key.Binding
+}
+
+type vaultKeyMap struct {
+	Create  key.Binding
+	Details key.Binding
+	Quit    key.Binding
+}
+
+type detailsKeyMap struct {
+	Back key.Binding
+	Edit key.Binding
+	Copy key.Binding
+}
+
+type formKeyMap struct {
+	Submit   key.Binding
+	Cancel   key.Binding
+	Next     key.Binding
+	Previous key.Binding
+	Generate key.Binding
+}
+
+func NewKeyMap() KeyMap {
+	return KeyMap{
+		Auth: authKeyMap{
+			Enter: key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "sign in")),
+			Quit:  key.NewBinding(key.WithKeys("ctrl+c"), key.WithHelp("ctrl + c", "exit")),
+		},
+		Vault: vaultKeyMap{
+			Create:  key.NewBinding(key.WithKeys("n"), key.WithHelp("n", "create new vault")),
+			Details: key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "show details")),
+			Quit:    key.NewBinding(key.WithKeys("ctrl+c"), key.WithHelp("ctrl + c", "exit")),
+		},
+		Details: detailsKeyMap{
+			Back: key.NewBinding(key.WithKeys("esc", "backspace"), key.WithHelp("esc", "back")),
+			Edit: key.NewBinding(key.WithKeys("e"), key.WithHelp("e", "edit")),
+			Copy: key.NewBinding(key.WithKeys("c"), key.WithHelp("c", "copy")),
+		},
+		Form: formKeyMap{
+			Submit: key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "save")),
+			Cancel: key.NewBinding(key.WithKeys("esc"), key.WithHelp("esc", "cancel")),
+			Next:   key.NewBinding(key.WithKeys("tab", "down"), key.WithHelp("tab", "down")),
+			Previous:   key.NewBinding(key.WithKeys("shift+tab", "up"), key.WithHelp("shift+tab", "up")),
+			Generate:    key.NewBinding(key.WithKeys("ctrl+g"), key.WithHelp("ctrl+g", "generate")),
+		},
+	}
+}
+
+func (k authKeyMap) ShortHelp() []key.Binding { return []key.Binding{k.Enter, k.Quit} }
+func (k detailsKeyMap) ShortHelp() []key.Binding { return []key.Binding{k.Back, k.Edit, k.Copy} }
+func (k formKeyMap) ShortHelp() []key.Binding { return []key.Binding{k.Submit, k.Generate, k.Cancel} }
