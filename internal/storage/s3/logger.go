5a34f1b5b4482b9958813e2e8ee2eb5e4aa27cfb
stepan41k <russia.zont@gmail.com>
2026-07-28 23:38:32 +0300

    feat(s3): add checksum exeption to s3 log

--- internal/storage/s3/logger.go
+++ internal/storage/s3/logger.go
@@ -1,9 +1,11 @@
 package s3
 
 import (
-    "github.com/aws/smithy-go/logging"
-    "log/slog"
-    "fmt"
+	"fmt"
+	"log/slog"
+	"strings"
+
+	"github.com/aws/smithy-go/logging"
 )
 
 type s3SlogAdapter struct {
@@ -11,6 +13,12 @@ type s3SlogAdapter struct {
 }
 
 func (a s3SlogAdapter) Logf(classification logging.Classification, format string, v ...interface{}) {
-    msg := fmt.Sprintf(format, v...)
-    a.log.Info(msg, "sdk_class", classification)
+	msg := fmt.Sprintf(format, v...)
+
+	// There is no need to verify the checksum, as it is verified at the encryption level
+	if strings.Contains(msg, "supported checksum") {
+		return
+	}
+
+	a.log.Info(msg, "sdk_class", classification)
 }
