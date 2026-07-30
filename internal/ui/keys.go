bcab9c6890718857782ce71267df5ce9b610615a
stepan41k <russia.zont@gmail.com>
2026-07-30 23:53:55 +0300

    feat: add keymaps for edit state

--- internal/ui/keys.go
+++ internal/ui/keys.go
@@ -8,7 +8,8 @@ type KeyMap struct {
 	Auth    authKeyMap
 	Vault   vaultKeyMap
 	Details detailsKeyMap
-	Form    formKeyMap
+	Create  createKeyMap
+	Edit    editKeyMap
 }
 
 type authKeyMap struct {
@@ -19,7 +20,7 @@ type authKeyMap struct {
 type vaultKeyMap struct {
 	Create  key.Binding
 	Details key.Binding
-	Edit key.Binding
+	Edit    key.Binding
 	Quit    key.Binding
 }
 
@@ -28,7 +29,15 @@ type detailsKeyMap struct {
 	Copy key.Binding
 }
 
-type formKeyMap struct {
+type createKeyMap struct {
+	Submit   key.Binding
+	Cancel   key.Binding
+	Next     key.Binding
+	Previous key.Binding
+	Generate key.Binding
+}
+
+type editKeyMap struct {
 	Submit   key.Binding
 	Cancel   key.Binding
 	Next     key.Binding
@@ -45,23 +54,30 @@ func NewKeyMap() KeyMap {
 		Vault: vaultKeyMap{
 			Create:  key.NewBinding(key.WithKeys("ctrl+n"), key.WithHelp("ctrl+n", "create new vault")),
 			Details: key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "show details")),
-			Edit: key.NewBinding(key.WithKeys("ctrl+e"), key.WithHelp("ctrl+e", "edit")),
+			Edit:    key.NewBinding(key.WithKeys("ctrl+e"), key.WithHelp("ctrl+e", "edit")),
 			Quit:    key.NewBinding(key.WithKeys("ctrl+c"), key.WithHelp("ctrl+c", "exit")),
 		},
 		Details: detailsKeyMap{
 			Back: key.NewBinding(key.WithKeys("esc", "backspace"), key.WithHelp("esc", "back")),
 			Copy: key.NewBinding(key.WithKeys("c"), key.WithHelp("c", "copy")),
 		},
-		Form: formKeyMap{
-			Submit: key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "save")),
-			Cancel: key.NewBinding(key.WithKeys("esc"), key.WithHelp("esc", "cancel")),
-			Next:   key.NewBinding(key.WithKeys("tab", "down"), key.WithHelp("tab", "down")),
-			Previous:   key.NewBinding(key.WithKeys("shift+tab", "up"), key.WithHelp("shift+tab", "up")),
-			Generate:    key.NewBinding(key.WithKeys("ctrl+g"), key.WithHelp("ctrl+g", "generate")),
+		Create: createKeyMap{
+			Submit:   key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "save")),
+			Cancel:   key.NewBinding(key.WithKeys("esc"), key.WithHelp("esc", "cancel")),
+			Next:     key.NewBinding(key.WithKeys("tab", "down"), key.WithHelp("tab", "down")),
+			Previous: key.NewBinding(key.WithKeys("shift+tab", "up"), key.WithHelp("shift+tab", "up")),
+			Generate: key.NewBinding(key.WithKeys("ctrl+g"), key.WithHelp("ctrl+g", "generate")),
+		},
+		Edit: editKeyMap{
+			Submit:   key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "save")),
+			Cancel:   key.NewBinding(key.WithKeys("esc"), key.WithHelp("esc", "cancel")),
+			Next:     key.NewBinding(key.WithKeys("tab", "down"), key.WithHelp("tab", "down")),
+			Previous: key.NewBinding(key.WithKeys("shift+tab", "up"), key.WithHelp("shift+tab", "up")),
+			Generate: key.NewBinding(key.WithKeys("ctrl+g"), key.WithHelp("ctrl+g", "generate")),
 		},
 	}
 }
 
-func (k authKeyMap) ShortHelp() []key.Binding { return []key.Binding{k.Enter, k.Quit} }
+func (k authKeyMap) ShortHelp() []key.Binding    { return []key.Binding{k.Enter, k.Quit} }
 func (k detailsKeyMap) ShortHelp() []key.Binding { return []key.Binding{k.Back, k.Copy} }
-func (k formKeyMap) ShortHelp() []key.Binding { return []key.Binding{k.Submit, k.Generate, k.Cancel} }
+func (k createKeyMap) ShortHelp() []key.Binding  { return []key.Binding{k.Submit, k.Generate, k.Cancel} }
