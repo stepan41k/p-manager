48473a32d28219ef04d73a9536c66e7a9fc9fec2
stepan41k <russia.zont@gmail.com>
2026-08-10 22:16:48 +0300

    feat(email): add otp email message

---
+++ internal/lib/email/email.go
@@ -0,0 +1,22 @@
+package email
+
+import (
+	"fmt"
+	"net/smtp"
+	"os"
+)
+
+func SendOTPEmail(code string) error {
+	from := os.Getenv("SENDER_EMAIL")
+		pass := os.Getenv("SENDER_PASSWORD")
+		to := os.Getenv("TARGET_EMAIL")
+		host := os.Getenv("SMTP_HOST")
+		port := os.Getenv("SMTP_PORT")
+
+		subject := "Subject: Vault Access Code\n"
+		body := fmt.Sprintf("Your one-time access code is: %s\nThis code will expire shortly.", code)
+		msg := []byte(subject + "\n" + body)
+
+		auth := smtp.PlainAuth("", from, pass, host)
+		return smtp.SendMail(host+":"+port, auth, from, []string{to}, msg)
+}
