c75f01936ea092d335601988e84e0f2dc10eaee6
stepan41k <russia.zont@gmail.com>
2026-08-12 23:53:13 +0300

    style(crypto): renamed file with password logic

--- internal/crypto/password.go
+++ internal/crypto/password.go
@@ -4,16 +4,16 @@ import "crypto/rand"
 
 func GeneratePassword(length int) string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
-	
+
 	b := make([]byte, length)
 	_, err := rand.Read(b)
 	if err != nil {
 		return ""
 	}
-	
+
 	for i := range b {
-		b[i] = charset[int(b[i]) % len(charset)]
+		b[i] = charset[int(b[i])%len(charset)]
 	}
-	
+
 	return string(b)
 }
