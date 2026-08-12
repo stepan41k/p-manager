e8225320ee07a143284206708239d1ddf139fe01
stepan41k <russia.zont@gmail.com>
2026-08-12 23:57:05 +0300

    style(crypto): formated aes

--- internal/crypto/aes.go
+++ internal/crypto/aes.go
@@ -17,52 +17,52 @@ func Encrypt(plaintext []byte, password []byte) ([]byte, error) {
 	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
 		return nil, err
 	}
-	
-	key := argon2.IDKey([]byte(password), salt, 1, 64 * 1024, 4, 32)
-	
+
+	key := argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
+
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	nonce := make([]byte, aesgcm.NonceSize())
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
 		return nil, err
 	}
-	
+
 	cipherText := aesgcm.Seal(nil, nonce, plaintext, nil)
-	
+
 	res := append(salt, nonce...)
 	res = append(res, cipherText...)
-	
+
 	return res, nil
 }
 
 func Decrypt(data []byte, password []byte) ([]byte, error) {
-	if len(data) < 16 + 12 {
+	if len(data) < 16+12 {
 		return nil, errors.New("invalid data length")
 	}
-	
+
 	salt := data[:16]
 	nonce := data[16:28]
 	cipherText := data[28:]
-	
-	key := argon2.IDKey([]byte(password), salt, 1, 64 * 1024, 4, 32)
-	
+
+	key := argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
+
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return aesgcm.Open(nil, nonce, cipherText, nil)
 }
