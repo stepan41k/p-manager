cbc42094fc171fb91b23e98d0022ffbef3646642
stepan41k <russia.zont@gmail.com>
2026-08-22 23:27:44 +0300

    refactor(crypto): update generate password function

--- internal/crypto/password.go
+++ internal/crypto/password.go
@@ -2,28 +2,68 @@ package crypto
 
 import (
 	"crypto/rand"
+	"errors"
 	"fmt"
 	"math/big"
 )
 
 const (
-	defaultCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+	lowerChars  = "abcdefghijklmnopqrstuvwxyz"
+	upperChars  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	digitChars  = "0123456789"
+	symbolChars = "!@#$%^&*()_+-=[]{}|;:,.<>?"
 )
 
-func GeneratePassword(length int) (string, error) {
-	if length <= 0 {
-		return "", fmt.Errorf("invalid password length: %d", length)
+type GeneratorOptions struct {
+	Length     int  `json:"length"`
+	UseLower   bool `json:"use_lower"`
+	UseUpper   bool `json:"use_upper"`
+	UseDigits  bool `json:"use_digits"`
+	UseSymbols bool `json:"use_symbols"`
+}
+
+func DefaultGeneratorOptions() GeneratorOptions {
+	return GeneratorOptions{
+		Length:     24,
+		UseLower:   true,
+		UseUpper:   true,
+		UseDigits:  true,
+		UseSymbols: true,
+	}
+}
+
+func GeneratePasswordWithOptions(opts GeneratorOptions) (string, error) {
+	if opts.Length <= 0 {
+		return "", errors.New("length must be greater than 0")
 	}
 
-	charsetLen := big.NewInt(int64(len(defaultCharset)))
-	result := make([]byte, length)
+	var charset string
+	if opts.UseLower {
+		charset += lowerChars
+	}
+	if opts.UseUpper {
+		charset += upperChars
+	}
+	if opts.UseDigits {
+		charset += digitChars
+	}
+	if opts.UseSymbols {
+		charset += symbolChars
+	}
 
-	for i := 0; i < length; i++ {
-		num, err := rand.Int(rand.Reader, charsetLen)
+	if charset == "" {
+		return "", errors.New("at least one character set must be selected")
+	}
+
+	charsetLen := big.NewInt(int64(len(charset)))
+	result := make([]byte, opts.Length)
+
+	for i := 0; i < opts.Length; i++ {
+		n, err := rand.Int(rand.Reader, charsetLen)
 		if err != nil {
-			return "", fmt.Errorf("failed to generate random character: %w", err)
+			return "", fmt.Errorf("failed to generate char: %w", err)
 		}
-		result[i] = defaultCharset[num.Int64()]
+		result[i] = charset[n.Int64()]
 	}
 
 	return string(result), nil
