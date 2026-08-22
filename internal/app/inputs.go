40cc162ac0a0d4e5eeb253c0838195efd45579bf
stepan41k <russia.zont@gmail.com>
2026-08-22 23:23:56 +0300

    feat(app/inputs): add setup for ty keymap configuration state

--- internal/app/inputs.go
+++ internal/app/inputs.go
@@ -1,6 +1,9 @@
 package app
 
-import "charm.land/bubbles/v2/textinput"
+import (
+	"charm.land/bubbles/v2/textinput"
+	"github.com/stepan41k/p-manager/internal/crypto"
+)
 
 func (m *Model) SetupInitialInputs() {
 	m.focusIndex = 0
@@ -55,6 +58,44 @@ func (m *Model) setupOTPInput() {
 	m.inputs[0] = t
 }
 
+func (m *Model) setupKeymapList() {
+	m.keymapIndex = 0
+	m.isRebinding = false
+
+	m.bindList = []ConfigurableKey{
+		{Category: "SETUP", Name: "Next", Binding: &m.keys.Setup.Next},
+		{Category: "SETUP", Name: "Previous", Binding: &m.keys.Setup.Previous},
+		{Category: "VAULT", Name: "Create", Binding: &m.keys.Vault.Create},
+		{Category: "VAULT", Name: "Delete", Binding: &m.keys.Vault.Delete},
+		{Category: "VAULT", Name: "Edit", Binding: &m.keys.Vault.Edit},
+		{Category: "DETAILS", Name: "Copy", Binding: &m.keys.Details.Copy},
+		{Category: "DETAILS", Name: "View", Binding: &m.keys.Details.View},
+		{Category: "CREATE", Name: "Next", Binding: &m.keys.Create.Next},
+		{Category: "CREATE", Name: "Previous", Binding: &m.keys.Create.Previous},
+		{Category: "CREATE", Name: "Generate", Binding: &m.keys.Create.Generate},
+		{Category: "EDIT", Name: "Next", Binding: &m.keys.Edit.Next},
+		{Category: "EDIT", Name: "Previous", Binding: &m.keys.Edit.Previous},
+		{Category: "EDIT", Name: "Generate", Binding: &m.keys.Edit.Generate},
+	}
+}
+
+func (m *Model) setupGenConfig() {
+	m.genOptIndex = 0
+
+	if m.config != nil && m.config.Generator.Length > 0 {
+		m.genOpts = m.config.Generator
+	} else {
+		m.genOpts = crypto.DefaultGeneratorOptions()
+	}
+
+	pass, err := crypto.GeneratePasswordWithOptions(m.genOpts)
+	if err == nil {
+		m.previewPass = pass
+	} else {
+		m.previewPass = "error generating preview"
+	}
+}
+
 func (m *Model) setupFormInputs(values []string) {
 	m.focusIndex = 0
 	m.inputs = make([]textinput.Model, 4)
@@ -65,7 +106,7 @@ func (m *Model) setupFormInputs(values []string) {
 		t.SetWidth(40)
 		t.Prompt = ""
 		t.Placeholder = placeholders[i]
-		
+
 		if len(values) > i {
 			if i == 3 {
 				t.EchoMode = textinput.EchoPassword
