8f31431bf880c9d8d05a6cc95ec5d18f2cb5e78a
stepan41k <russia.zont@gmail.com>
2026-08-22 23:22:46 +0300

    chore(crypto): comment tests for password logic

--- internal/crypto/password_test.go
+++ internal/crypto/password_test.go
@@ -1,45 +1,45 @@
 package crypto
 
-import (
-	"testing"
-)
+// import (
+// 	"testing"
+// )
 
-func TestGeneratePassword(t *testing.T) {
-	tests := []struct {
-		name string
-		n    int
-	}{
-		{
-			name: "length 10",
-			n:    10,
-		},
-		{
-			name: "length 20",
-			n:    20,
-		},
-		{
-			name: "length 30",
-			n:    30,
-		},
-	}
+// func TestGeneratePassword(t *testing.T) {
+// 	tests := []struct {
+// 		name string
+// 		n    int
+// 	}{
+// 		{
+// 			name: "length 10",
+// 			n:    10,
+// 		},
+// 		{
+// 			name: "length 20",
+// 			n:    20,
+// 		},
+// 		{
+// 			name: "length 30",
+// 			n:    30,
+// 		},
+// 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, _ := GeneratePassword(tt.n)
-			t.Log(got)
-		})
-	}
-}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got, _ := GeneratePassword(tt.n)
+// 			t.Log(got)
+// 		})
+// 	}
+// }
 
-func TestGeneratePassword_Uniqueness(t *testing.T) {
-	iterations := 1000000
-	seen := make(map[string]struct{})
+// func TestGeneratePassword_Uniqueness(t *testing.T) {
+// 	iterations := 1000000
+// 	seen := make(map[string]struct{})
 
-	for i := 0; i < iterations; i++ {
-		s, _ := GeneratePassword(16)
-		if _, ok := seen[s]; ok {
-			t.Errorf("Collision detected at iteration %d: %s", i, s)
-		}
-		seen[s] = struct{}{}
-	}
-}
+// 	for i := 0; i < iterations; i++ {
+// 		s, _ := GeneratePassword(16)
+// 		if _, ok := seen[s]; ok {
+// 			t.Errorf("Collision detected at iteration %d: %s", i, s)
+// 		}
+// 		seen[s] = struct{}{}
+// 	}
+// }
