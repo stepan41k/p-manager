1f8e9de5d6b1dc416419cd5e9e574a00bfc4cafc
stepan41k <russia.zont@gmail.com>
2026-08-22 14:08:46 +0300

    feat(storage/s3): update trusted device logic

--- internal/storage/s3/model.go
+++ internal/storage/s3/model.go
@@ -1,7 +1,16 @@
 package s3
 
+import "time"
+
+type TrustedDevice struct {
+	Hash      string    `json:"hash"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
 type Metadata struct {
-	Salt                []byte   `json:"salt"`
-	Verifier            []byte   `json:"verifier"`
-	TrustedDeviceHashes []string `json:"trusted_device_hashes"`
+	Salt                []byte          `json:"salt"`
+	Verifier            []byte          `json:"verifier"`
+	TrustedDevices[]TrustedDevice `json:"trusted_devices"`
 }
