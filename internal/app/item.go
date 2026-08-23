5b2b072bc908445b613a8e5070ed397a18ee64ff
stepan41k <russia.zont@gmail.com>
2026-08-23 12:54:15 +0300

    feat(app/item): added Note field to item

--- internal/app/item.go
+++ internal/app/item.go
@@ -1,12 +1,15 @@
 package app
 
+import "fmt"
+
 type VaultItem struct {
 	Resource string `json:"resource"`
 	Email    string `json:"email"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Note     string `json:"note,omitempty"`
 }
 
-func (i VaultItem) FilterValue() string { return i.Resource }
+func (i VaultItem) FilterValue() string { return fmt.Sprintf("%s %s", i.Resource, i.Note) }
 func (i VaultItem) Title() string       { return i.Resource }
 func (i VaultItem) Description() string { return i.Email + " " + i.Username }
