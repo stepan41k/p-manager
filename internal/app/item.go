ccd54174ef2533aa1229b6c86654b51796f80c2e
stepan41k <russia.zont@gmail.com>
2026-08-24 22:04:16 +0300

    feat(app/item): added logic to track duplicate passwords

--- internal/app/item.go
+++ internal/app/item.go
@@ -1,15 +1,35 @@
 package app
 
-import "fmt"
+import (
+	"fmt"
+
+	"charm.land/bubbles/v2/list"
+)
 
 type VaultItem struct {
-	Resource string `json:"resource"`
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Note     string `json:"note,omitempty"`
+	Resource    string `json:"resource"`
+	Email       string `json:"email"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Note        string `json:"note,omitempty"`
+	IsDuplicate bool   `json:"-"`
+}
+
+func GetVaultItem(it list.Item) (VaultItem, bool) {
+	if v, ok := it.(VaultItem); ok {
+		return v, true
+	}
+	if v, ok := it.(*VaultItem); ok && v != nil {
+		return *v, true
+	}
+	return VaultItem{}, false
 }
 
 func (i VaultItem) FilterValue() string { return fmt.Sprintf("%s %s", i.Resource, i.Note) }
-func (i VaultItem) Title() string       { return i.Resource }
+func (i VaultItem) Title() string {
+	if i.IsDuplicate {
+		return i.Resource + " ⚠️"
+	}
+	return i.Resource
+}
 func (i VaultItem) Description() string { return i.Email + " " + i.Username }
