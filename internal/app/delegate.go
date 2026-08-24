772cab6f75a5a724af471c6cab710f52e98e668d
stepan41k <russia.zont@gmail.com>
2026-08-24 22:02:46 +0300

    feat(app/delegate): update delegate

--- internal/app/delegate.go
+++ internal/app/delegate.go
@@ -1,36 +1,43 @@
 package app
 
-// import (
-// 	"fmt"
-// 	"io"
-// 	"strings"
+import (
+	"fmt"
+	"io"
 
-// 	"charm.land/bubbles/v2/list"
-// 	tea "charm.land/bubbletea/v2"
-// 	"github.com/stepan41k/p-manager/internal/ui/styles"
-// )
+	"charm.land/bubbles/v2/list"
+	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
+)
 
-// type itemDelegate struct {
-// 	styles styles.Styles
-// }
+type itemDelegate struct {
+	styles Styles
+}
 
-// func (d *itemDelegate) Height() int                             { return 1 }
-// func (d *itemDelegate) Spacing() int                            { return 0 }
-// func (d *itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-// func (d *itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-// 	i, ok := listItem.(VaultItem)
-// 	if !ok {
-// 		return
-// 	}
+func (d *itemDelegate) Height() int                             { return 1 }
+func (d *itemDelegate) Spacing() int                            { return 0 }
+func (d *itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := GetVaultItem(listItem)
+	if !ok {
+		return
+	}
 
-// 	str := fmt.Sprintf("%d. %s", index+1, i.Resource)
+	title := i.Resource
 
-// 	fn := d.styles.Item.Render
-// 	if index == m.Index() {
-// 		fn = func(s ...string) string {
-// 			return d.styles.SelectedItem.Render("> " + strings.Join(s, " "))
-// 		}
-// 	}
+	if i.IsDuplicate {
+		warningBadge := lipgloss.NewStyle().
+			Foreground(lipgloss.Color("208")).
+			Bold(true).
+			Render(" ⚠️")
 
-// 	fmt.Fprint(w, fn(str))
-// }
+		title = title + warningBadge
+	}
+
+	str := fmt.Sprintf("%s\n%s %s", title, i.Email, i.Username)
+
+	if index == m.Index() {
+		fmt.Fprint(w, d.styles.SelectedItem.Render("> "+str))
+	} else {
+		fmt.Fprint(w, d.styles.Item.Render("  "+str))
+	}
+}
