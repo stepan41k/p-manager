f47212b3fb02106310e031e03017b76c7d5a7a3c
stepan41k <russia.zont@gmail.com>
2026-08-25 16:41:03 +0300

    chore(app/delegate): commented code

--- internal/app/delegate.go
+++ internal/app/delegate.go
@@ -1,43 +1,43 @@
 package app
 
-import (
-	"fmt"
-	"io"
+// import (
+// 	"fmt"
+// 	"io"
 
-	"charm.land/bubbles/v2/list"
-	tea "charm.land/bubbletea/v2"
-	"charm.land/lipgloss/v2"
-)
+// 	"charm.land/bubbles/v2/list"
+// 	tea "charm.land/bubbletea/v2"
+// 	"charm.land/lipgloss/v2"
+// )
 
-type itemDelegate struct {
-	styles Styles
-}
+// type itemDelegate struct {
+// 	styles Styles
+// }
 
-func (d *itemDelegate) Height() int                             { return 1 }
-func (d *itemDelegate) Spacing() int                            { return 0 }
-func (d *itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := GetVaultItem(listItem)
-	if !ok {
-		return
-	}
+// func (d *itemDelegate) Height() int                             { return 1 }
+// func (d *itemDelegate) Spacing() int                            { return 0 }
+// func (d *itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+// func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+// 	i, ok := GetVaultItem(listItem)
+// 	if !ok {
+// 		return
+// 	}
 
-	title := i.Resource
+// 	title := i.Resource
 
-	if i.IsDuplicate {
-		warningBadge := lipgloss.NewStyle().
-			Foreground(lipgloss.Color("208")).
-			Bold(true).
-			Render(" ⚠️")
+// 	if i.IsDuplicate {
+// 		warningBadge := lipgloss.NewStyle().
+// 			Foreground(lipgloss.Color("208")).
+// 			Bold(true).
+// 			Render(" ⚠️")
 
-		title = title + warningBadge
-	}
+// 		title = title + warningBadge
+// 	}
 
-	str := fmt.Sprintf("%s\n%s %s", title, i.Email, i.Username)
+// 	str := fmt.Sprintf("%s\n%s %s", title, i.Email, i.Username)
 
-	if index == m.Index() {
-		fmt.Fprint(w, d.styles.SelectedItem.Render("> "+str))
-	} else {
-		fmt.Fprint(w, d.styles.Item.Render("  "+str))
-	}
-}
+// 	if index == m.Index() {
+// 		fmt.Fprint(w, d.styles.SelectedItem.Render("> "+str))
+// 	} else {
+// 		fmt.Fprint(w, d.styles.Item.Render("  "+str))
+// 	}
+// }
